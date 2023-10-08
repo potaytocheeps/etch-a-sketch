064676d40565fe019953a1bb7db1959a3a55a507
Potayto <83526209+potaytocheeps@users.noreply.github.com>
2023-10-07 17:35:30 -0700

    Start and stop the painting process when user clicks on grid

--- script.js
+++ script.js
@@ -16,10 +16,31 @@ for (let rowCounter = 1; rowCounter <= gridSize; rowCounter++)
     gridContainer.appendChild(row);
 }
 
+function paintGridCells(event)
+{
+    event.target.style.backgroundColor = '#000';
+}
+
 const gridCells = document.querySelectorAll('.grid-cell');
 
-gridCells.forEach((gridCell) => {
-    gridCell.addEventListener('mouseenter', (event) => {
-        event.target.style.backgroundColor = '#000';
-    })
-})
+// One click on the grid will start the painting process. Another click on the grid will
+// stop it. This happens by toggling the painting class on the grid container on each click
+gridContainer.addEventListener('click', (event) => {
+    event.currentTarget.classList.toggle('painting');
+
+    if (gridContainer.classList.contains('painting'))
+    {
+        // Only paint target if it is a grid cell. This fixes a bug where whole rows
+        // or the entire grid are painted if the user clicks and drags on the container
+        if (event.target.classList.contains('grid-cell'))
+        {
+            event.target.style.backgroundColor = '#000';
+        }
+
+        gridCells.forEach((gridCell) => gridCell.addEventListener('mouseenter', paintGridCells));
+    }
+    else
+    {
+        gridCells.forEach((gridCell) => gridCell.removeEventListener('mouseenter', paintGridCells));
+    }
+});
